575df7faf57974f97fa326ca99ba5199fa12d11e
Willian <willian@bytebr.com.br>
2022-02-09 15:53:54 -0300

    Incomplete code challenge for Hacker Rank: a solution for the disk space analysis

---
+++ DiskSpaceAnalysis/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var space = new List<int>(){1,5,1,2,3,1,2};
+var x=7;
+var maxSpace=0;
+for(int i=0;i<x-1;i++)
+{
+    var min = Math.Min(space[i], space[i+1]);
+    if (min>maxSpace)
+    maxSpace=min;
+}
+Console.WriteLine(maxSpace);
