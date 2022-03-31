b4bab6be37ba6c06f1bd4aead9616414e9945796
William Severino <william@willcodes.com>
2022-03-31 08:34:07 -0300

    first commit

---
+++ CalculateChange/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppToptal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = getChange(5, 0.99M); // should return [1,0,0,0,0,4]
+            printResult(res);
+
+            res = getChange(3.14M, 1.99M);// should return [0,1,1,0,0,1]
+            printResult(res);
+
+            res = getChange(3, 0.01M); // should return [4,0,2,1,1,2]
+            printResult(res);
+
+            res = getChange(4, 3.14M); // should return [1,0,1,1,1,0]
+            printResult(res);
+
+            res = getChange(0.45M, 0.34M); // should return [1,0,1,0,0,0]
+            printResult(res);
+        }
+        private static void printResult(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("");
+        }
+
+        private static int[] getChange(decimal M, decimal P)
+        {
+            var coins = new List<int>() { 100, 50, 25, 10, 5, 1};
+            var remaining = (M-P)*100;
+            var change = new int[6];
+            var position = coins.Count();
+            foreach (var item in coins)
+            {
+                if (remaining >= item) // (5-0.99)*100 = 401 < 100
+                {
+                    var residual = remaining % item;
+                    int qt = (int)(remaining-residual)/ item;
+                    remaining = remaining - (qt * item);
+
+                    change[position-1] = qt;
+                }
+                position--;
+            }
+            return change;
+        }
+    }
+}
---
+++ FragmentedNames/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppToptal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var result = findWord(new string[] { "P>E", "E>R", "R>U" }); // PERU
+            //Console.WriteLine(result);
+            
+            //result = findWord(new string[] { "I>N", "A>I", "P>A", "S>P" }); // SPAIN
+            //Console.WriteLine(result);
+
+            //result = findWord(new string[] { "U>N", "G>A", "R>Y", "H>U", "N>G", "A>R" }); // HUNGARY
+            //Console.WriteLine(result);
+
+            //result = findWord(new string[]{"I>F", "W>I", "S>W", "F>T"}); // SWIFT
+            //Console.WriteLine(result);
+
+            var result = findWord(new string[] { "R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G" }); // PORTUGAL
+            Console.WriteLine(result);
+
+            //result = findWord(new string[] { "W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T" }); // SWITZERLAND
+            //Console.WriteLine(result);
+
+
+            Console.ReadKey();
+        }
+
+        private static string findWord(string[] parts)
+        {
+            string resultWord = "";
+            foreach (var part in parts)
+            {
+                //if (resultWord.Length >= parts.Count())
+                //    break;
+
+                var posFirst = resultWord.IndexOf(part[0]);
+                var posSecond = resultWord.IndexOf(part[2]);
+                
+                if (posFirst == -1 && posSecond == -1)
+                {
+                    resultWord += string.Concat(part[0], part[2]);
+                }
+                else if (posFirst ==-1)
+                {
+                    //in -> AI
+                    //the first character must be put before the second
+                    var startWord = "";
+                    if (posSecond>0)
+                    {
+                        startWord = resultWord.Substring(0, posSecond); 
+                    }
+                    resultWord = startWord + part[0] + resultWord.Substring(posSecond);
+                }
+                else if (posSecond ==-1)
+                {
+                    //the second char must be put after the second afeter the first position
+                    resultWord = resultWord.Substring(0, posFirst) + resultWord.Substring(posFirst) + part[2];
+                }
+            }
+            return resultWord;
+        }
+    }
+}
